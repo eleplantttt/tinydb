b766ac70ac06eaa1f7dfe6b4806837ba5894c765
cxb <1206296077@qq.com>
2023-05-01 15:32:47 +0800

    修改：     src/include/primer/p0_starter.h

--- src/include/primer/p0_starter.h
+++ src/include/primer/p0_starter.h
@@ -9,7 +9,7 @@
 // Copyright (c) 2015-2020, Carnegie Mellon University Database Group
 //
 //===----------------------------------------------------------------------===//
-//cute test
+//cute test cute
 #pragma once
 
 #include <memory>
