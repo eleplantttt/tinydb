80b535210485e549b30b4b88b08045a2a8a99e5b
cxb <1206296077@qq.com>
2023-07-04 22:55:32 +0800

    seq_scan_executor(wxx)

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -11,13 +11,136 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/seq_scan_executor.h"
+#include <cassert>
+#include "common/config.h"
+#include "concurrency/lock_manager.h"
+#include "concurrency/transaction.h"
+
+
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan)
+    : AbstractExecutor(exec_ctx),
+      plan_(plan),
+      table_info_(exec_ctx->GetCatalog()->GetTable(plan->GetTableOid())),
+      cur_(nullptr, {}, nullptr),
+      cur_page_id_(INVALID_PAGE_ID) {}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() {
+  cur_ = table_info_->table_->Begin(exec_ctx_->GetTransaction());
+  cur_page_id_ = INVALID_PAGE_ID;
+  LockTable();
+}
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  const auto &end = table_info_->table_->End();
+  const auto &bpm = exec_ctx_->GetBufferPoolManager();
+
+  while (cur_ != end) {
+    // TODO(cxb) optimize lurk
+    // lru_k实现有问题，
+    const auto page_id = cur_->GetRid().GetPageId();
+    if (page_id != cur_page_id_) {
+      if (cur_page_id_ != INVALID_PAGE_ID) {
+        bpm->UnpinPage(cur_page_id_, false);
+      }
+      cur_page_id_ = page_id;
+      bpm->FetchPage(cur_page_id_);
+    }
+
+    *rid = cur_->GetRid();
+
+    LockRow(*rid);
+    *tuple = *cur_;
+    UnLockRow(*rid);
+
+    cur_++;
+    if (plan_->filter_predicate_ != nullptr) {
+      const auto value = plan_->filter_predicate_->Evaluate(tuple, plan_->OutputSchema());
+      if (value.IsNull() || !value.GetAs<bool>()) {
+        continue;
+      }
+    }
+    // fmt::print("SeqScanExecutor::Next {}\n", tuple->ToString(&plan_->OutputSchema()));
+
+    return false;
+  }
+
+  if (cur_page_id_ != INVALID_PAGE_ID) {
+    bpm->UnpinPage(cur_page_id_, false);
+  }
+
+  UnLockTable();
+  return false;
+}
+
+void SeqScanExecutor::LockTable() {
+#ifndef SEQNLOCK
+  const auto &txn = exec_ctx_->GetTransaction();
+  const auto &lock_mgr = exec_ctx_->GetLockManager();
+  const auto &oid = plan_->table_oid_;
+  bool res = true;
+
+  try {
+    if (txn->GetIsolationLevel() != IsolationLevel::READ_UNCOMMITTED) {
+      if (!txn->IsTableIntentionExclusiveLocked(oid) && !txn->IsTableSharedIntentionExclusiveLocked(oid)) {
+        res = lock_mgr->LockTable(txn, LockManager::LockMode::INTENTION_SHARED, oid);
+      }
+    }
+    if (!res) {
+      assert(txn->GetState() == TransactionState::ABORTED);
+      throw ExecutionException("SeqScanExecutor::Init() lock fail");
+    }
+  } catch (TransactionAbortException &e) {
+    assert(txn->GetState() == TransactionState::ABORTED);
+    throw ExecutionException("SeqScanExecutor::Init() lock fail");
+  }
+#endif
+}
+
+void SeqScanExecutor::UnLockTable() {
+#ifndef SEQNLOCK
+  const auto &txn = exec_ctx_->GetTransaction();
+  const auto &lock_mgr = exec_ctx_->GetLockManager();
+  const auto &oid = plan_->GetTableOid();
+  if (txn->GetIsolationLevel() == IsolationLevel::READ_COMMITTED) {
+    if (txn->IsTableIntentionSharedLocked(oid)) {
+      lock_mgr->UnlockTable(txn, oid);
+    }
+  }
+#endif
+}
+
+void SeqScanExecutor::LockRow(const RID &rid) {
+#ifndef SEQNLOCK
+  const auto &txn = exec_ctx_->GetTransaction();
+  const auto &lock_mgr = exec_ctx_->GetLockManager();
+  const auto &oid = plan_->GetTableOid();
+  bool res = true;
+  if (txn->GetIsolationLevel() != IsolationLevel::READ_UNCOMMITTED) {
+    if (!txn->IsRowExclusiveLocked(oid, rid)) {
+      res = lock_mgr->LockRow(txn, LockManager::LockMode::SHARED, oid, rid);
+    }
+  }
+  if (!res) {
+    txn->SetState(TransactionState::ABORTED);
+    throw ExecutionException("SeqScanExecutor::Next() lock fail");
+  }
+#endif
+}
+
+void SeqScanExecutor::UnLockRow(const RID &rid) {
+#ifndef SEQNLOCK
+  const auto &txn = exec_ctx_->GetTransaction();
+  const auto &lock_mgr = exec_ctx_->GetLockManager();
+  const auto &oid = plan_->GetTableOid();
+  if (txn->GetIsolationLevel() == IsolationLevel::READ_COMMITTED) {
+    if (txn->IsRowSharedLocked(oid, rid)) {
+      lock_mgr->UnlockRow(txn, oid, rid);
+    }
+  }
+#endif
+}
 
 }  // namespace bustub
--- src/include/execution/executors/seq_scan_executor.h
+++ src/include/execution/executors/seq_scan_executor.h
@@ -14,9 +14,13 @@
 
 #include <vector>
 
+#include "common/config.h"
+#include "common/rid.h"
 #include "execution/executor_context.h"
 #include "execution/executors/abstract_executor.h"
 #include "execution/plans/seq_scan_plan.h"
+#include "storage/table/table_heap.h"
+#include "storage/table/table_iterator.h"
 #include "storage/table/tuple.h"
 
 namespace bustub {
@@ -50,5 +54,13 @@ class SeqScanExecutor : public AbstractExecutor {
  private:
   /** The sequential scan plan node to be executed */
   const SeqScanPlanNode *plan_;
+  const TableInfo *table_info_;
+  TableIterator cur_;
+  page_id_t cur_page_id_;
+
+  void LockTable();
+  void UnLockTable();
+  void LockRow(const RID &rid);
+  void UnLockRow(const RID &rid);
 };
 }  // namespace bustub
